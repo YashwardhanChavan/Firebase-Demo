c8ece7c3546237ba158f48b4dd797db0f72762dc
YashwardhanChavan <chavanyashwardhan@gmail.com>
2020-11-12 14:03:38 +0530

    Firebase authentication demo.

--- app/src/main/java/com/example/firebasedemo/LoginActivity.java
+++ app/src/main/java/com/example/firebasedemo/LoginActivity.java
@@ -2,13 +2,41 @@ package com.example.firebasedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText mEditTextEmail;
+    private EditText mEditTextPassword;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        auth = FirebaseAuth.getInstance();
+    }
+
+    public void loginUser(View view) {
+        String email = mEditTextEmail.getText().toString();
+        String password = mEditTextPassword.getText().toString();
+        login(email, password);
+    }
+    public void login(String email, String password){
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                finish();
+            }
+        });
     }
 }
